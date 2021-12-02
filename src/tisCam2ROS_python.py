850bdd256c0d85e11acfd07e8021c15c9a09fb32
haroldmurcia <harold.murcia@unibague.edu.co>
2021-12-02 20:17:14 +0000

    python

--- src/tisCam2ROS_python.py
+++ src/tisCam2ROS_python.py
@@ -14,37 +14,40 @@ import gi
 print("Python version:\t", sys.version)
 print ("CV2 version:\t", cv2.__version__)
 #camset_var="v4l2src device=/dev/video0 ! video/x-raw, format=BGRx,width=1280, height=960, framerate=30/1 ! videoconvert ! appsink"
-#camset_var="v4l2src device=/dev/video0 ! video/x-raw, width=(int)1280,height=(int)960, framerate=(fraction)60/1 ! videoconvert  ! video/x-raw, format=(string)BGR ! appsink"
+#camset_var="tcambin ! video/x-raw, width=1280, height=960, framerate=30/1 ! videoconvert  ! appsink sync=false name=sink"
+
 #camset_var="tcambin serial=26810384 ! video/x-raw, width=1280,height=960, framerate=30/1 ! videoconvert ! appsink"
-camset_var="tcambin serial=26810384 ! video/x-raw, format=BGRx, width=640,height=480, framerate=60/1 ! videoconvert ! appsink"
+#camset_var="tcambin serial=26810384 ! video/x-raw, format=BGRx, width=640,height=480, framerate=30/1 ! videoconvert ! appsink"
 #cam = cv2.VideoCapture(camset_var,cv2.CAP_GSTREAMER)
-cam = cv2.VideoCapture(camset_var,cv2.CAP_GSTREAMER)
-print("open?",cam.isOpened())
+cam = cv2.VideoCapture(0)
+cam.open(0)
+print("is cam open?:", cam.isOpened())
 cam.release()
 
-print(camset_var)
+#print(camset_var)
+w, h = 1280, 960
 def image_bridge():
     image_pub = rospy.Publisher("image_topic", Image, queue_size=10)
     rospy.init_node('image_bridge', anonymous=True)
     rate = rospy.Rate(60)
-    cam = cv2.VideoCapture(camset_var,cv2.CAP_GSTREAMER)
-    #cam.set(cv2.CAP_PROP_FPS, 60)
-    #cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-    #cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 960)
-    #cam.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter.fourcc('G','R','B','G'))
-    #cam.set(cv2.CAP_PROP_CONVERT_RGB, True)
-    #print(cam.get(cv2.CAP_PROP_AUTO_EXPOSURE))
-    #print(cam.get(cv2.CAP_PROP_FORMAT))
+    cam = cv2.VideoCapture(0)
+    cam.set(cv2.CAP_PROP_FPS, 30)
+    cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+    cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 960)
+    cam.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('G','R','A','Y'))
+    cam.set(cv2.CAP_PROP_CONVERT_RGB, False)
+    print(cam.get(cv2.CAP_PROP_FPS))
+    print(cam.get(cv2.CAP_PROP_FORMAT))
     #print(cam.get(cv2.CAP_PROP_MODE))
-    #print(cam.get(cv2.CAP_PROP_CONVERT_RGB))
+    print(cam.get(cv2.CAP_PROP_CONVERT_RGB))
     bridge = CvBridge()
     while not rospy.is_shutdown() and cam.isOpened():
         ret,frame = cam.read()
-        cv2.imwrite('capture.png', frame)
-        #frame = cv2.cvtColor(frame, cv2.COLOR_BayerGR2BGR)
+        #cv2.imwrite('capture.png', frame)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BayerGB2GRAY)
         image_message = bridge.cv2_to_imgmsg(frame, "passthrough")
         image_pub.publish(image_message)
-        #cv2.imshow('img',frame)
+        cv2.imshow('img',frame)
         rate.sleep()
         if cv2.waitKey(1)==ord('q'):
             break
